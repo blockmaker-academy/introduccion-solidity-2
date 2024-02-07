4f8d54992c3cf4ff8e177c6a0d279de3362ff56a
IvánManso <imanso.arenal@gmail.com>
2024-02-07 21:07:30 +0100

    RegistroDeEmpleados.sol & .js => added

--- 01. Ejercicios/04. Volumen 4/test/AlmacenamientoSimple.test.js
+++ 01. Ejercicios/04. Volumen 4/test/AlmacenamientoSimple.test.js
@@ -9,14 +9,15 @@ contract('AlmacenamientoSimple', (cuentas) => {
     almacenamientoSimple = await AlmacenamientoSimple.new();
   });
 
-  it('debería establecer y obtener datos', async () => {
-    const nuevosDatos = 42;
+  // Individual Test (it)
+  it('Test [1]: Establecer y obtener datos', async () => {
 
     // Establecer datos
-    await almacenamientoSimple.establecerDatos(nuevosDatos);
+    await almacenamientoSimple.establecerDatos(42);
 
     // Obtener datos y afirmar
     const resultado = await almacenamientoSimple.obtenerDatos();
-    expect(resultado.toNumber()).to.equal(nuevosDatos);
+    expect(resultado.toNumber()).to.equal(42);
   });
+
 });
